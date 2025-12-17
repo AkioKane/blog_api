ece0d9237581c3287f0479cdb3fdf1ff0747c043
Akio Kane <aakocherga@mail.ru>
2025-12-17 22:54:10 +0700

    Added scroll container

--- client/user_blog/src/routes/Home.tsx
+++ client/user_blog/src/routes/Home.tsx
@@ -1,5 +1,6 @@
 import type React from 'react';
 import { HeaderHome } from '../components/Header';
+import scrollArrow from "../assets/scrollArrow.svg";
 import styles from './Home.module.css';
 
 interface HomeProps {}
@@ -10,7 +11,13 @@ const Home: React.FC<HomeProps> = () => {
       <div className={styles.homeContainer}>
         <HeaderHome />
         
-        <div className={styles.heroContainer}></div>
+        <div className={styles.heroContainer}>
+          <div className={styles.scrollContainer}>
+            <img src={scrollArrow} alt="scroll" />
+
+            <h3>scroll down</h3>
+          </div>
+        </div>
 
         <div className={styles.blackSection}></div>
       </div>
